d955c75db774a1dd15f96858cb89f4ae90e2b39d
Massawe14 <ramadhanimassawe14@gmail.com>
2022-05-17 15:57:05 +0300

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
             <x-jet-authentication-card-logo />
@@ -45,4 +45,43 @@
             </div>
         </form>
     </x-jet-authentication-card>
+</x-guest-layout> --}}
+
+<x-guest-layout>
+   <div class="container-scroller">
+      <div class="container-fluid page-body-wrapper full-page-wrapper">
+        <div class="row w-100 m-0">
+          <div class="content-wrapper full-page-wrapper d-flex align-items-center auth login-bg">
+            <div class="card col-lg-4 mx-auto">
+              <div class="card-body px-5 py-5">
+                <h3 class="card-title text-left mb-3">Login</h3>
+                <x-jet-validation-errors class="mb-4" />
+                <form method="POST" action="{{route('login')}}">
+                  @csrf
+                  <div class="form-group">
+                    <label>Email Address</label>
+                    <input type="email" class="form-control p_input" id="" name="email" placeholder="Enter your email address" :value="old('email')" required autofocus>
+                  </div>
+                  <div class="form-group">
+                    <label>Password</label>
+                    <input type="password" class="form-control p_input" id="" name="password" placeholder="Enter your password" required autocomplete="current-password">
+                  </div>
+                  <div class="form-group d-flex align-items-center justify-content-between">
+                    <div class="form-check">
+                      <label class="form-check-label">
+                        <input type="checkbox" class="form-check-input" name="remember" value="forever"> Remember me </label>
+                    </div>
+                    <a href="{{route('password.request')}}" class="forgot-pass">Forgot password</a>
+                  </div>
+                  <div class="text-center">
+                    <button type="submit" class="btn btn-primary btn-block enter-btn">Login</button>
+                  </div>
+                  <p class="sign-up">Don't have an Account?<a href="{{route('register')}}"> Sign Up</a></p>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 </x-guest-layout>
