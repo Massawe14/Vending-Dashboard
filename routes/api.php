52f31fd93e3d01bfc8aeab59c77cbeb9fa5a73c5
Massawe14 <ramadhanimassawe14@gmail.com>
2022-05-18 13:50:23 +0300

    update api

--- routes/api.php
+++ routes/api.php
@@ -29,7 +29,7 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/products', [ProductController::class, 'store']);
     Route::put('/products/{id}', [ProductController::class, 'update']);
     Route::delete('/products{id}', [ProductController::class, 'destroy']);
-    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/logout', [AuthController::class, 'logout']); 
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
