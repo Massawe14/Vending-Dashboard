c6451d719154b465328bb03316ae0dff68a029c1
Massawe14 <ramadhanimassawe14@gmail.com>
2022-05-20 17:26:33 +0300

    update

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -15,7 +15,7 @@ class ProductsController extends Controller
     public function index()
     {
         $product = Product::all()->toArray();
-        return view('livewire.user.product-list-component', compact('product'))->layout('layouts.base');
+        return view('livewire.user.product-list-component', compact('product'));
     }
 
     /**
