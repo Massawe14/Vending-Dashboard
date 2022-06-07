8a94844912d74a29694b08e3eeef9db59d377515
Massawe14 <ramadhanimassawe14@gmail.com>
2022-06-07 09:02:42 +0300

    update

--- resources/views/livewire/user/vending-product-component.blade.php
+++ resources/views/livewire/user/vending-product-component.blade.php
@@ -304,10 +304,10 @@
             <!-- content-wrapper ends -->
             <!-- partial:partials/_footer.html -->
             <footer class="footer">
-                <div class="d-sm-flex justify-content-center justify-content-sm-between">
-                    <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © imperialinnovations.co.tz 2022</span>
-                    <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"><a href="https://imperialinnovations.co.tz/projects/vending-machine.html" target="_blank">Vending machine</a> from imperialinnovations.co.tz</span>
-                </div>
+              <div class="d-sm-flex justify-content-center justify-content-sm-between">
+                <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © imperialinnovations.co.tz 2022</span>
+                <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"><a href="https://imperialinnovations.co.tz/projects/vending-machine.html" target="_blank">Vending machine</a> from imperialinnovations.co.tz</span>
+              </div>
             </footer>
             <!-- partial -->
         </div>
